ee43b6f121bfbb033beac766039e6101ad72968a
choko <ro22113176@gmail.com>
2021-04-07 15:10:42 +0800

    DJ second

---
+++ mysite/DJtest_/views.py
@@ -0,0 +1,10 @@
+
+from datetime import datetime
+from django.shortcuts import render
+
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
+# Create your views here.
